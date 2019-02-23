87857b260dbd714c8efb99ab64bb682d751fc695
Tony Gaskell <tony@sudokrew.com>
2019-02-22 19:57:42 -1000

    Add contentful plugin and offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,29 +16,29 @@ module.exports = {
         name: "images",
         path: path.join(__dirname, "src", "images")
       }
-    }
-    // {
-    //   resolve: "gatsby-source-contentful",
-    //   options: {
-    //     spaceId: process.env.CONTENTFUL_SPACE_ID,
-    //     accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN
-    //   }
-    // },
-    // "gatsby-transformer-remark",
-    // {
-    //   resolve: "gatsby-plugin-manifest",
-    //   options: {
-    //     name: "hello-contentful-gatsby",
-    //     short_name: "contentful-gatsby",
-    //     start_url: "/",
-    //     background_color: "#663399",
-    //     theme_color: "#663399",
-    //     display: "minimal-ui",
-    //     icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site
-    //   }
-    // }
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN
+      }
+    },
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "hello-contentful-gatsby",
+        short_name: "contentful-gatsby",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // "gatsby-plugin-offline"
+    "gatsby-plugin-offline"
   ]
 };
